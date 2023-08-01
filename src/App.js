f5e5aca1b596c9465f836c310f93ab4b90adc8cf
Erkan <erkankolakan@gmail.com>
2023-08-01 17:49:08 +0300

    scroll yapıldı

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import { ScrollControls, Scroll } from '@react-three/drei';
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
 
 function App() {
   return (
-    <div className="App">
-      Selam Dunya
-    </div>
+    <>
+    <color attach='background' args={['#000000']} />
+
+  <ScrollControls pages={6} damping={0.25} >
+      <Scroll>
+
+      </Scroll>
+
+      <Scroll html style={{width:'100%'}}>
+        
+      </Scroll>
+      
+  </ScrollControls>
+    </>
   );
 }
 
 export default App;
+
+
+
+ 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import logo from './img/logo-2.png';
+import { Canvas } from '@react-three/fiber';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <header>
+      <img src={logo} alt="logo"    />
+    </header>
+    <Canvas>
+      <App />
+    </Canvas>
   </React.StrictMode>
 );
 
