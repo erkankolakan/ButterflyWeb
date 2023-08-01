64f23b1f8fea86abc1eeed5e7771363d7d2b9d3a
Erkan <erkankolakan@gmail.com>
2023-08-01 19:20:41 +0300

    fln

--- src/App.js
+++ src/App.js
@@ -16,7 +16,16 @@ function App() {
       </Scroll>
 
       <Scroll html style={{width:'100%'}}>
-        
+        <Container style={{position:"relative"}} >
+          <Row className='text-center align-items-center justify-content-center' style={{position:"absolute", width:"100vw", height:"100vh padding:0px 30px " }}>
+            <Col xs={6}>
+              <div>
+                <h1 style={{marginBottom:"0px"}}>Life can be a struggle</h1>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+
       </Scroll>
       
   </ScrollControls>
